9c2a33fad8308a2784f2bc8336b5c4d3aa6c409a
Kasun Nirmala <bgkasunnirmala@gmail.com>
2020-05-27 12:45:06 +0530

    thingspeak

--- app.js
+++ app.js
@@ -1,26 +1,20 @@
 var net = require('net');
 const express = require('express');
 const app = express();
+const https = require('https'); 
 
-app.get('/getBy/:txt', (request, response) => {
 
-    client.get(request.params.txt, function (error, result) {
-        if (error) {
-            console.log(error);
-            response.send([]);
-        }
-        response.send(result);
-    });
-});
-
-app.listen(1112);
+// app.listen(1112);
 
 var tcp = net.createServer(function (soc) {
     console.log("CONNECTED");
     soc.setKeepAlive(true);
     soc.on('data', function (data) {
-        console.log(data);
-        
+        // console.log(data);
+        console.log(parseFloat(data.toString()));
+        https.get('https://api.thingspeak.com/update?api_key=5ACCSSP1X1S2WSCN&field1=' + parseFloat(data.toString())).on("error", (err) => {
+            console.log("Error: " + err.message);
+        });;
     });
 }).listen(1111);
 console.log("SERVER STARTED");
---
+++ test.js
@@ -0,0 +1,2 @@
+const https = require('https'); 
+https.get('https://api.thingspeak.com/update?api_key=5ACCSSP1X1S2WSCN&field1=100');
