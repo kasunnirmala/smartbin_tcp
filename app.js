e9bf88d2459cee4a0e05585ab88d40c0980e69db
Kasun Nirmala <bgkasunnirmala@gmail.com>
2020-07-29 12:14:49 +0530

    socketio

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 
-
 var net = require('net');
 const express = require('express');
 const app = express();
+require('./db');
+var io = require('socket.io').listen(4567);
 const https = require('https');
 const DeviceModel = require('./model/device');
 
-// app.listen(1112);
+var socket;
+
+io.on('connection', sockt => {
+    console.log("CONNECTED");
+    socket=sockt;
+    socket.emit('new-message', 123);
+});
+
+
 
 var tcp = net.createServer(function (soc) {
     console.log("CONNECTED");
@@ -16,10 +25,6 @@ var tcp = net.createServer(function (soc) {
         //var val=parseInt(data.toString('hex'),16);
         var val = parseFloat(data.toString('utf8'));
         console.log(val);
-        https.get('https://api.thingspeak.com/update?api_key=5ACCSSP1X1S2WSCN&field1=' + val).on("error", (err) => {
-            console.log("Error: " + err.message);
-        });;
-
 
         const Device = new DeviceModel({
             deviceID: "Device001",
@@ -28,17 +33,25 @@ var tcp = net.createServer(function (soc) {
 
         try {
             const savedDevice = await Device.save();
+            console.log(savedDevice);
         } catch (error) {
-            console.log(error.message );
+            console.log(error.message);
 
         };
+
+        socket.emit('new-message', val);
+
+        // https.get('https://api.thingspeak.com/update?api_key=5ACCSSP1X1S2WSCN&field1=' + val).on("error", (err) => {
+        //     console.log("Error: " + err.message);
+        // });;
+
+
+       
 
 
 
     });
 }).listen(1111);
 console.log("SERVER STARTED");
-
-
 
 
--- test.js
+++ test.js
@@ -1,2 +1,12 @@
-const https = require('https'); 
-https.get('https://api.thingspeak.com/update?api_key=5ACCSSP1X1S2WSCN&field1=100');
+
+
+// const server = require('http').createServer();
+// const io = require('socket.io')(server);
+var io = require('socket.io').listen(3000);
+io.on('connection', client => {
+    console.log("CONNECTED");
+    client.emit('new-message', "ASD");
+    console.log("SEND");
+
+});
+// server.listen(3000);
