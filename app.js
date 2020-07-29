314a6da56876429f63c4096c1568ca65747e64e0
Kasun Nirmala <bgkasunnirmala@gmail.com>
2020-07-29 12:28:52 +0530

    scoket null

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var socket;
 
 io.on('connection', sockt => {
     console.log("CONNECTED");
-    socket=sockt;
+    socket = sockt;
     socket.emit('new-message', 123);
 });
 
@@ -20,7 +20,7 @@ io.on('connection', sockt => {
 var tcp = net.createServer(function (soc) {
     console.log("CONNECTED");
     soc.setKeepAlive(true);
-    soc.on('data',async function (data) {
+    soc.on('data', async function (data) {
         // console.log(data.toString('utf8'));
         //var val=parseInt(data.toString('hex'),16);
         var val = parseFloat(data.toString('utf8'));
@@ -38,15 +38,15 @@ var tcp = net.createServer(function (soc) {
             console.log(error.message);
 
         };
-
-        socket.emit('new-message', val);
-
+        if (socket) {
+            socket.emit('new-message', val);
+        }
         // https.get('https://api.thingspeak.com/update?api_key=5ACCSSP1X1S2WSCN&field1=' + val).on("error", (err) => {
         //     console.log("Error: " + err.message);
         // });;
 
 
-       
+
 
 
 
