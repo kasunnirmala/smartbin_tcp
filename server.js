8a81da9bac1456d978b377e5f6e3440e39e1fd86
Kasun Nirmala <bgkasunnirmala@gmail.com>
2020-07-30 02:05:55 +0530

    http fix

--- server.js
+++ server.js
@@ -1,12 +1,39 @@
 const express = require('express');
 const app = express();
-
+var io = require('socket.io').listen(4567);
+const DeviceModel = require('./model/device');
 require('./db');
 
-app.get('/value/:val', async (req, res) => {
-  
-    console.log(req.params.categoryID);
+var socket;
 
+io.on('connection', sockt => {
+    console.log("IO CONNECTED");
+    socket = sockt;
+    socket.emit('new-message', 123);
+});
+
+
+
+
+app.get('/value/:valu', async (req, res) => {
+  
+    var val = parseFloat(req.params.valu.toString('utf8'));
+    console.log(val);
+
+    const Device = new DeviceModel({
+        deviceID: "Device001",
+        value: val
+    });
+
+    try {
+        const savedDevice = await Device.save();
+        console.log(savedDevice);
+    } catch (error) {
+        console.log(error.message);
+
+    };
+
+    io.emit('new-message', val);
 
 });
 
